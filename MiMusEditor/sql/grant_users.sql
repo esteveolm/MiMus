084ba8f2b5af8e9daa9fd5769ed1cda9b39f85d2
javierbj <javierbeltranj@gmail.com>
2019-05-14 15:45:48 +0200

    Created DB script that grants table permissions to users.

---
+++ MiMusEditor/sql/grant_users.sql
@@ -0,0 +1,7 @@
+GRANT SELECT, INSERT, UPDATE, DELETE ON Mimus.Bibliografia TO mimus01@localhost;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Mimus.Artista TO mimus01@localhost;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Mimus.Casa TO mimus01@localhost;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Mimus.Instrument TO mimus01@localhost;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Mimus.Lloc TO mimus01@localhost;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Mimus.Ofici TO mimus01@localhost;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Mimus.Promotor TO mimus01@localhost;
