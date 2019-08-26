79253023409d39190f7b2092461c4b32d4c7b0d5
javierbj <javierbeltranj@gmail.com>
2019-08-26 10:40:48 +0200

    Dialogs for entity annotation now show entities ordered alphabetically.

--- MiMusEditor/src/ui/dialog/InstanceDialog.java
+++ MiMusEditor/src/ui/dialog/InstanceDialog.java
@@ -1,5 +1,7 @@
 package ui.dialog;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.eclipse.swt.widgets.Composite;
@@ -12,6 +14,14 @@ public abstract class InstanceDialog<E extends Entity> extends EditorDialog<E> {
 	
 	public InstanceDialog(List<E> entities, Shell parentShell) {
 		super(entities, parentShell);
+		
+		/* Dialog shows entities ordered alphabetically by lemma */
+		Collections.sort(entities, new Comparator<E>() {
+			@Override
+			public int compare(E e1, E e2) {
+				return e1.getLemma().compareTo(e2.getLemma());
+			}
+		});
 	}
 	
 	@Override
